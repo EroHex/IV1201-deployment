2acb80a70e0cfc4834cf6a42d3f0d02df9c97291
Mikael <mikael.rudenvald@hotmail.se>
2025-02-19 19:27:34 +0100

    Update to JobApplication & new service
    Added a new JobApplicationService. Nothing there yet, but it is intended to communicate with the database when a jobapplication happens.
    
    In JobApplication: added getters and setters, imported jakarta.persistance.* and added relevant JPA annotations

--- Recruitment/src/main/java/project/com/Recruitment/model/JobApplication.java
+++ Recruitment/src/main/java/project/com/Recruitment/model/JobApplication.java
@@ -1,21 +1,64 @@
 package project.com.Recruitment.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class JobApplication {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id; //should this perhaps be public to connect with the other?
-    private ApplicationStatus status; //The current status of the application, which can be Accepted, Rejected, or Unhandeled, as per application-description.pdf
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus status;
+
     private String competency; //Is competency even a word? This is for the would be drop-down menu of industries applicant has experience in
     private int availability; //Period the applicant can work. Thinking int for which weeks during summer, i.e w.26-30
 
 
-    //Making a constructor, just as in the User-class. Should the enum actually be here?
+
     public JobApplication (Long id, ApplicationStatus status, String competency, int availability){
         this.id = id;
         this.status = status;
         this.competency = competency;
         this.availability = availability;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ApplicationStatus status) {
+        this.status = status;
+    }
+
+    public String getCompetency(){
+        return competency;
+    }
+
+    public void setCompetency(String competency) {
+        this.competency = competency;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
 }
 
 enum ApplicationStatus{
-    ACCEPTED, REJECTED, UNHANDLED
+    //For the Admin handling the job application
+    ACCEPTED, REJECTED, UNHANDLED,
+    //For the job applicant
+    SUBMITTED, CANCELLED, IN_PROGRESS
 }
---
+++ Recruitment/src/main/java/project/com/Recruitment/service/JobApplicationService.java
@@ -0,0 +1,8 @@
+package project.com.Recruitment.service;
+
+/*The point of this file is to save the application, step by step according to the description.
+* If the user goes back it should fetch what was already written (say they press cancel - send them back to page 1 to edit what they've written
+* The user can submit what they've entered at the last page
+* */
+public class JobApplicationService {
+}
