278bbe533869d75aa9e491b58db9f84c92a2b23c
MrBregott <108457182+MrBregott@users.noreply.github.com>
2025-02-21 04:12:08 +0100

    Renamed index.html to account.html, added more information shown on the profile view,  added buttons to go to create-application page or manage-application page depending on role_id value, changed controller to use static final strings for view names

--- Recruitment/src/main/java/project/com/Recruitment/controller/PersonController.java
+++ Recruitment/src/main/java/project/com/Recruitment/controller/PersonController.java
@@ -19,68 +19,114 @@ import jakarta.servlet.http.HttpSession;
 // @RequestMapping("/person")
 public class PersonController {
 
+    static final String DEFAULT_PAGE_URL = "/";
+    static final String LOGIN_PAGE_URL = "login";
+    static final String REGISTER_PAGE_URL = "register";
+    static final String LOGOUT_PAGE_URL = "logout";
+    static final String PROFILE_PAGE_URL = "account";
+    static final String MANAGEAPPLICATIONS_PAGE_URL = "manage-applications";
+    static final String CREATEAPPLICATION_PAGE_URL = "create-application";
+
     @Autowired
     private PersonService personService;
 
     // login via login.html filen 
-    
-    @GetMapping("/")
-    public String homePage(Model model, HttpSession session) {
-        if (session.getAttribute("loggedInUser") == null) {
-            return "redirect:/login"; // hittar filen i src/main/resources/templates/login.html
-        }
-        return "index"; // hittar filen i src/main/resources/templates/index.html
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("loggedInUser") != null;
     }
 
-    @GetMapping("/login")
+    private boolean isAdmin(HttpSession session) {
+        Person person = (Person) session.getAttribute("loggedInUser");
+        if (person.getRoleId() == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    @GetMapping(DEFAULT_PAGE_URL)
+    public String homePage(Model model, HttpSession session) {
+        if (!(isLoggedIn(session))) {
+            return "redirect:" + DEFAULT_PAGE_URL + LOGIN_PAGE_URL;
+        }
+        return "redirect:" + DEFAULT_PAGE_URL + PROFILE_PAGE_URL;
+    }
+
+    @GetMapping(DEFAULT_PAGE_URL + PROFILE_PAGE_URL)
+    public String profilePage(Model model, HttpSession session) {
+        if (!(isLoggedIn(session))) {
+            return "redirect:" + DEFAULT_PAGE_URL + LOGIN_PAGE_URL;
+        }
+        return PROFILE_PAGE_URL;
+    }
+
+    @GetMapping(DEFAULT_PAGE_URL + LOGIN_PAGE_URL)
     public String loginPage(Model model) {
         model.addAttribute("loginDTO", new LoginDTO());
-        return "login"; // hittar filen i src/main/resources/templates/login.html
+        return LOGIN_PAGE_URL; // hittar filen i src/main/resources/templates/login.html
     }
 
-    @GetMapping("/register")
+    @GetMapping(DEFAULT_PAGE_URL + REGISTER_PAGE_URL)
     public String registerPage(Model model) {
         model.addAttribute("registerDTO", new RegisterDTO());
-        return "register"; // hittar filen i src/main/resources/templates/register.html
+        return REGISTER_PAGE_URL; // hittar filen i src/main/resources/templates/register.html
     }
 
-    @PostMapping("/login")
+    @PostMapping(DEFAULT_PAGE_URL + LOGIN_PAGE_URL)
     public String login(@Valid LoginDTO loginDTO, BindingResult bindingResult, HttpSession session, Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("error", "Validation failed: " + bindingResult.getAllErrors());
-            return "login";
+            return LOGIN_PAGE_URL;
         }
         boolean validUser = personService.validateUser(loginDTO); //skicka till service för databas hantering
         if (validUser) {
             Person person = personService.getPersonByUsername(loginDTO.getUsername());
             session.setAttribute("loggedInUser", person);
             System.out.println("User logged in: " + person);
-            return "redirect:/"; // Redirect to home page after successful login
+            return "redirect:" + DEFAULT_PAGE_URL; // Redirect to home page after successful login
         } else {
             model.addAttribute("error", "Invalid username or password. Please try again.");
-            return "login"; // Return to login page with error message
+            return LOGIN_PAGE_URL; // Return to login page with error message
         }
     }
 
-    @PostMapping("/logout")
+    @PostMapping(DEFAULT_PAGE_URL + LOGOUT_PAGE_URL)
     public String logout(HttpSession session) {
         session.invalidate(); // Invalidate the session to log out the user
-        return "redirect:/login"; // Redirect to login page after logout
+        return "redirect:" + DEFAULT_PAGE_URL + LOGOUT_PAGE_URL; // Redirect to login page after logout
     }
     
     // register account via register.html filen
-    @PostMapping("/register")
+    @PostMapping(DEFAULT_PAGE_URL + REGISTER_PAGE_URL)
     public String register(@Valid RegisterDTO registerDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "register";
+            return REGISTER_PAGE_URL;
         }
 
         try {
             personService.registerPerson(registerDTO); //skicka till service för databas hantering
-            return "redirect:/login";
+            return "redirect:" + DEFAULT_PAGE_URL + LOGIN_PAGE_URL; // Redirect to login page after successful registration
         } catch (RuntimeException e) {
-            return "register";      // add error handling
+            return REGISTER_PAGE_URL;      // add error handling
         }
+    }
+
+    @GetMapping(DEFAULT_PAGE_URL + MANAGEAPPLICATIONS_PAGE_URL)
+    public String manageApplications(Model model, HttpSession session) {
+        if (!(isLoggedIn(session))) {
+            return "redirect:" + DEFAULT_PAGE_URL + LOGIN_PAGE_URL;
+        }
+        if (!(isAdmin(session))) {
+            return "redirect:" + DEFAULT_PAGE_URL;
+        }
+        return MANAGEAPPLICATIONS_PAGE_URL;
+    }
+
+    @GetMapping(DEFAULT_PAGE_URL + CREATEAPPLICATION_PAGE_URL)
+    public String createApplication(Model model, HttpSession session) {
+        if (!(isLoggedIn(session))) {
+            return "redirect:" + DEFAULT_PAGE_URL + LOGIN_PAGE_URL;
+        }
+        return CREATEAPPLICATION_PAGE_URL;
     }
 }
 
--- Recruitment/src/main/java/project/com/Recruitment/security/SecurityConfig.java
+++ Recruitment/src/main/java/project/com/Recruitment/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package project.com.Recruitment.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,8 +14,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/login", "/register").permitAll() // tillåter just nu alla användare att gå vart som helst, måste fixas
-                .anyRequest().permitAll() // används när alla användare ska vara authenticated
+                .anyRequest().permitAll()
             );
         return http.build();
     }
