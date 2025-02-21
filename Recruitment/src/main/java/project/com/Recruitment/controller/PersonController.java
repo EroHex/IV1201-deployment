bc89842c52888be9374562af130c249a85f594d2
EroHex <william030428@gmail.com>
2025-02-21 01:16:32 +0100

    Changes to personcontroller and register.html
    mapped register.html to registerdto object and removed script section so thymeleaf handles it, changed register mapping in controller.

--- Recruitment/src/main/java/project/com/Recruitment/controller/PersonController.java
+++ Recruitment/src/main/java/project/com/Recruitment/controller/PersonController.java
@@ -38,12 +38,13 @@ public class PersonController {
     }
 
     @GetMapping("/register")
-    public String registerPage() {
+    public String registerPage(Model model) {
+        model.addAttribute("registerDTO", new RegisterDTO());
         return "register"; // hittar filen i src/main/resources/templates/register.html
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginDTO loginDTO, BindingResult bindingResult, HttpSession session, Model model) {
+    public String login(@Valid LoginDTO loginDTO, BindingResult bindingResult, HttpSession session, Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("error", "Validation failed: " + bindingResult.getAllErrors());
             return "login";
@@ -61,16 +62,16 @@ public class PersonController {
     }
     // register account via register.html filen
     @PostMapping("/register")
-    public ResponseEntity<String> register(@Valid @ModelAttribute RegisterDTO registerDTO, BindingResult bindingResult) {
+    public String register(@Valid RegisterDTO registerDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return ResponseEntity.badRequest().body("Validation failed: " + bindingResult.getAllErrors());
+            return "register";
         }
 
         try {
             personService.registerPerson(registerDTO); //skicka till service för databas hantering
-            return ResponseEntity.ok("Person Registered Successfully");
+            return "redirect:/login";
         } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return "register";      // add error handling
         }
     }
 }
